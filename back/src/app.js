b72f46597117dc347d012c619c8a90a3c0190901
joel-CM <cjoel1977@gmail.com>
2021-11-14 11:03:40 -0300

    finished product model & back

--- back/src/app.js
+++ back/src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-// rutas
+// <----------impport routs---------->
 const routeClients = require("./routes/clients.route");
+const routeProducts = require("./routes/product.route");
 
-// middlewares
+// <----------middlewares---------->
 app.use(express.json());
 app.use(morgan("dev"));
 
 // routs -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 app.use("/api/clients", routeClients);
+app.use("/api/products", routeProducts);
 
 module.exports = app;
--- back/src/models/Product.model.js
+++ back/src/models/Product.model.js
@@ -4,12 +4,15 @@ module.exports = (sequelize) => {
   sequelize.define("Product", {
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
       type: DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: 000,
     },
   }),
     {
-      TimeRanges: false,
+      timestmaps: false,
     };
 };
